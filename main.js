14f43839ebd9cb3f61082adc96045f38771581e0
Shyam Sundar Velmurugan <145095417+HwuShyam2005@users.noreply.github.com>
2024-07-31 16:11:56 +0400

    Delete main.js

--- main.js
+++
@@ -1,5 +0,0 @@
-const toggleBtn = document.querySelector(".toggle_btn")
-const menu = document.querySelector(".menu")
-toggleBtn.addEventListener("click" , () => {
-  menu.classList.toggle("open")
-})
